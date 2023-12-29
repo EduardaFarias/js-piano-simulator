dd0c9d8f2ca6a5ba00dabeceb2648254d8355a39
EduardaFarias <97049617+EduardaFarias@users.noreply.github.com>
2023-12-29 18:52:29 -0300

    Feat: Validating Piano Keys

---
+++ src/js/engine.js
@@ -0,0 +1,27 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+let mapedKeys = [];
+let audio = new Audio("src/tunes/a.wav");
+
+const playTune = (key) => {
+    audio.src = `src/tunes/${key}.wav`
+    audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(()=>{
+        clickedKey.classList.remove("active");
+    },150);
+
+};
+
+pianoKeys.forEach((key) => {
+    key.addEventListener("click", ()=> playTune(key.dataset.key));
+    mapedKeys.push(key.dataset.key);
+});
+
+
+document.addEventListener("keydown", (e) => {
+    if(mapedKeys.includes(e.key)){
+    playTune(e.key);
+    }
+});
